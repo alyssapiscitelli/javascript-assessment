aa4501a2af9afa0f227bd1c975c9ae530c2b84c1
alyssapiscitelli <122324506+alyssapiscitelli@users.noreply.github.com>
2023-02-06 20:31:55 -0500

    Fixed Variables
    Added extra information

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let firstName='Alyssa'; 
-var lastName='Piscitelli';
-const myAge=31;
-alert ('My name is Alyssa Piscitelli');
-alert('I am 31 years old');
+let lastName='Piscitelli';
+let myAge='31';
+alert ('My name is '+ firstName + ' '+lastName);
+alert('I am '+ myAge+' years old');
 console.log ("I am having a lot of fun in the class I am enjoying leanring everything in visual studio code");
