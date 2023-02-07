a0ad5d9c82e50c37fc488fffafd0330200ba02a9
alyssapiscitelli <122324506+alyssapiscitelli@users.noreply.github.com>
2023-02-06 19:36:13 -0500

    fixed javascript issue
    Wrote in the proper syntax

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-console.log ("I am having a lot of fun in the class I am enjoying leanring everything in visual studio code");
+let firstName='Alyssa'; 
+var lastName='Piscitelli';
+const myAge=31;
 alert ('My name is Alyssa Piscitelli');
 alert('I am 31 years old');
+console.log ("I am having a lot of fun in the class I am enjoying leanring everything in visual studio code");
